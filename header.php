f14c8be47bcada14fb76d9f1ed1a54f2c5fe2e1c
Daniel Ribeiro <danarrib@gmail.com>
2023-06-30 23:45:43 -0300

    Fix: Pages titles

--- header.php
+++ header.php
@@ -1,7 +1,16 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
 <HTML>
 <HEAD>
-    <TITLE><?php bloginfo('name'); ?></TITLE>
+    <TITLE><?php
+        if(is_front_page() || is_home())
+        {
+			echo get_bloginfo('name');
+		}
+        else
+        {
+			echo wp_title('') . ' | ' . get_bloginfo('name');
+		}
+        ?></TITLE>
     <META HTTP-EQUIV="Content-Type" CONTENT="<?php bloginfo('html_type'); ?>;charset=ISO-8859-1"></META>
     <META NAME="viewport" CONTENT="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
